dee76331b11573ed27768840531bda6e3d9ff613
alexgpitts <alexgpitts@gmail.com>
2021-02-07 13:57:02 -0800

    finished
    added tests

---
+++ full_name.py
@@ -0,0 +1,14 @@
+'''
+Author: Alex Pitts
+Date: 2/7/21
+Class: CS 362
+
+Description: This program simply returns a full name given a first and last name 
+'''
+
+def get_full_name(first, last):
+    full = first + " " + last
+
+    return full
+
+# print(get_full_name("Alex", "Pitts"))
---
+++ test_full_name.py
@@ -0,0 +1,17 @@
+import unittest
+import full_name
+
+class TestCase(unittest.TestCase):
+    
+    #check if it will return exact first string and second string separated by a space
+    def test_my_name(self):
+        self.assertEqual(full_name.get_full_name("Alex", "Pitts"), "Alex Pitts")
+
+    #check if it will uppercase names
+    def test_my_name_lower_case(self):
+        self.assertEqual(full_name.get_full_name("alex", "pitts"), "Alex Pitts")
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
